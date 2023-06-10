7c2bfb5c83080fd44bd81c0adbb257ea238ef71e
EhsanPazhooman <ehspazh@gmail.com>
2023-06-10 06:50:36 -0700

    create a project

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+    ///////////////System Pause Method//////////////////////
+    public static void pressEnterToContinue()
+    {
+        System.out.println("Press Enter key to continue...");
+        try
+        {
+            System.in.read();
+        }
+        catch(Exception e)
+        {}
+    }
+}
+
+
